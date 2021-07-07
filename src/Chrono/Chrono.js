8ee00966a02c6d6a05786a86ab5ea02beed33b0d
sechs <philippe.buteau@skema.edu>
2021-07-07 13:02:55 +0200

    logic created

--- src/Chrono/Chrono.js
+++ src/Chrono/Chrono.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 import "./Chrono.css";
 import PauseImg from "../Images/pause.svg";
 import PlayImg from "../Images/play.svg";
@@ -12,6 +12,38 @@ export default function Chrono() {
   const [breakTimeFixed, setBreakTimeFixed] = useState(300);
 
   const [workingChrono, setWorkingChrono] = useState(false);
+  const [state, dispatch] = useReducer(reducer);
+
+  function reducer(state, action) {
+    // eslint-disable-next-line default-case
+    switch (action.type) {
+      case "TICK":
+        if (sessionTime >= 0) {
+          setSessionTime(sessionTime - 1);
+        } else if (breakTime >= 1) {
+          setBreakTime(breakTime - 1);
+        } else if (breakTime <= 0 && breakTime <= 0) {
+          setSessionTime(sessionTimeFixed);
+          setBreakTime(setBreakTimeFixed);
+        }
+    }
+  }
+
+  useEffect(() => {
+    let id;
+
+    if (workingChrono) {
+      id = window.setInterval(() => dispatch({ type: "TICK" }), 1000);
+    }
+
+    return () => {
+      window.clearInterval(id);
+    };
+  }, [workingChrono]);
+
+  const playPause = () => {
+    setWorkingChrono(!workingChrono);
+  };
 
   return (
     <div className="container-chrono">
@@ -31,7 +63,7 @@ export default function Chrono() {
         <span>CHRONO</span>
       </h1>
       <div className="container-controllers">
-        <button>
+        <button onClick={playPause}>
           <img src={workingChrono ? PauseImg : PlayImg} alt="play" />
         </button>
         <button>
