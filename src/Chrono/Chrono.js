ff2d7b69cb283cca1dea91ea896a1e2b01ef7b7f
sechs <philippe.buteau@skema.edu>
2021-07-07 13:45:23 +0200

    finished

--- src/Chrono/Chrono.js
+++ src/Chrono/Chrono.js
@@ -28,6 +28,7 @@ export default function Chrono() {
         }
     }
   }
+  //   console.log(sessionTime, breakTime);
 
   useEffect(() => {
     let id;
@@ -45,28 +46,86 @@ export default function Chrono() {
     setWorkingChrono(!workingChrono);
   };
 
+  const handleSession = (e) => {
+    const el = e.target;
+    if (el.classList.contains("minus")) {
+      if (sessionTime / 60 > 1) {
+        setSessionTime(sessionTime - 60);
+        setSessionTimeFixed(sessionTimeFixed - 60);
+      }
+    } else if (el.classList.contains("plus")) {
+      setSessionTime(sessionTime + 60);
+      setSessionTimeFixed(sessionTimeFixed + 60);
+    }
+  };
+
+  const handleBreak = (e) => {
+    const el = e.target;
+    if (el.classList.contains("minus")) {
+      if (breakTime / 60 > 1) {
+        setBreakTime(breakTime - 60);
+        setBreakTimeFixed(breakTimeFixed - 60);
+      }
+    } else if (el.classList.contains("plus")) {
+      setBreakTime(breakTime + 60);
+      setBreakTimeFixed(breakTimeFixed + 60);
+    }
+  };
+
+  const resetFunc = () => {
+    if (workingChrono) {
+      setWorkingChrono(!workingChrono);
+    }
+    setSessionTime(sessionTimeFixed);
+    setBreakTime(breakTimeFixed);
+  };
+
   return (
-    <div className="container-chrono">
+    <div
+      className={
+        workingChrono ? "container-chrono anim-glow" : "container-chrono"
+      }
+    >
       <div className="container-config">
         <div className="box-btns session">
-          <button className="minus">-</button>
+          <button onClick={handleSession} className="minus">
+            -
+          </button>
           <span>{sessionTimeFixed / 60}</span>
-          <button className="plus">+</button>
+          <button onClick={handleSession} className="plus">
+            +
+          </button>
         </div>
         <div className="box-btns break">
-          <button className="minus">-</button>
+          <button onClick={handleBreak} className="minus">
+            -
+          </button>
           <span>{breakTimeFixed / 60}</span>
-          <button className="plus">+</button>
+          <button onClick={handleBreak} className="plus">
+            +
+          </button>
         </div>
       </div>
+
       <h1>
-        <span>CHRONO</span>
+        {sessionTime >= 0 ? (
+          <span>{`${Math.trunc(sessionTime / 60)} : ${
+            sessionTime % 60 < 10
+              ? `0${sessionTime % 60}`
+              : `${sessionTime % 60}`
+          }`}</span>
+        ) : (
+          <span>{`${Math.trunc(breakTime / 60)} : ${
+            breakTime % 60 < 10 ? `0${breakTime % 60}` : `${breakTime % 60}`
+          }`}</span>
+        )}
       </h1>
+
       <div className="container-controllers">
         <button onClick={playPause}>
           <img src={workingChrono ? PauseImg : PlayImg} alt="play" />
         </button>
-        <button>
+        <button onClick={resetFunc}>
           <img src={ResetImg} alt="reset" />
         </button>
       </div>
